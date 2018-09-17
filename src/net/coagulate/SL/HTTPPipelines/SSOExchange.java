8b3854f0d1aaa473f04f966ab7432761a442c73b
Iain Price (LUNA) <git@predestined.net>
2018-09-17 20:39:44 +0100

    Test of SSL

---
+++ src/net/coagulate/SL/HTTPPipelines/SSOExchange.java
@@ -0,0 +1,80 @@
+package net.coagulate.SL.HTTPPipelines;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import net.coagulate.SL.Log;
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpRequestHandler;
+
+/**
+ *
+ * @author Iain Price
+ */
+public class SSOExchange implements HttpRequestHandler {
+   @Override
+    public void handle(HttpRequest req, HttpResponse resp, HttpContext hc) {
+        try {
+            Map<String,String> parameters=new HashMap<>();
+            if (req instanceof HttpEntityEnclosingRequest) {
+                HttpEntityEnclosingRequest r=(HttpEntityEnclosingRequest) req;
+                List<NameValuePair> map = URLEncodedUtils.parse(r.getEntity());
+                for (NameValuePair kv:map) {
+                    parameters.put(kv.getName(),kv.getValue());
+                }
+            }
+            State state=new State();
+            state.request=req;
+            state.response=resp;
+            state.httpcontext=hc;
+            state.parameters=parameters;
+            Map<String,String> cookiemap=new HashMap<>();
+            Header cookies = req.getFirstHeader("Cookie");
+            if (cookies!=null) {
+                for (HeaderElement cookieelement:cookies.getElements()) {
+                    cookiemap.put(cookieelement.getName(), cookieelement.getValue());
+                }
+            }
+            state.cookies=cookiemap;
+            state.sessionid=cookiemap.get("coagulateslsessionid");
+            resp.setEntity(new StringEntity(header()+handleString(state)+footer(),ContentType.TEXT_HTML));
+            if (state.sessionid!=null) {
+                if (!state.sessionid.equals(cookiemap.get("coagulateslsessionid"))) {
+                    resp.addHeader("Set-Cookie","coagulateslsessionid="+state.sessionid+"; HttpOnly; Path=/; Domain=coagulate.net;");
+                }
+            }
+            resp.setStatusCode(HttpStatus.SC_OK);
+            return;
+        } catch (Exception ex) {
+            Log.warn("StringHandler","Unexpected exception thrown in page handler",ex);
+            resp.setStatusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+            resp.setEntity(new StringEntity("<html><body><pre><b>500 - Internal Server Error</b></pre><p>Internal Exception, see debug logs</p></body></html>",ContentType.TEXT_HTML));
+            return;
+        }
+    }    
+
+    protected String handleString(State state) {
+        return "Hello I'm the SSO Exchange";
+    }
+    
+    protected String header() {
+        return "<html><head><title>Coagulate SL Services</title></head><body>"
+                + "<h1 align=center>Coagulate SL Services</h1><p><hr>"
+                + "Hello<span style='display:block;float:right;'>There</span>"
+                + "<hr></p>";
+    }
+    protected String footer() {
+        return "<div style='position:absolute;bottom:5;right:5;left:5;'><hr><span style='display:block;float:right;'>(C) Iain Maltz @ Second Life</span></div></body></html>";
+    }
+    
+}

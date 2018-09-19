3511e24022066f044e8f2399f2c6ec0516f014f2
Iain Price (LUNA) <git@predestined.net>
2018-09-20 00:46:53 +0100

    now can set password oO

--- src/net/coagulate/SL/Pages/SetPassword.java
+++ src/net/coagulate/SL/Pages/SetPassword.java
@@ -17,9 +17,15 @@ public class SetPassword extends Page {
         State state=State.get();
         if (state.get("Set Password").equals("Set Password"))
         {
-            para("Setting your password to "+state.get("password"));
-            para("Have a nice day :P");
-            return;
+            String password=state.get("password");
+            boolean ok=true;
+            if (password.length()<6) { error("Use a longer password"); }
+            else {
+                para("Setting your password");            
+                state.user().setPassword(password);
+                para("Password is now set!");
+                return;
+            }
         }
         para("Here you may set a password which you may use to log directly in to this application<br>without having to log in to Second Life and message a bot.");
         para("As a matter of general security you should use a unique password,<br>and definately <b>NOT</b> the same password you use for Second Life");

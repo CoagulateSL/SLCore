6dafbe21681cc7baf1fb27b4b79e5a7a83de4052
Iain Price (LUNA) <git@predestined.net>
2018-09-20 00:13:59 +0100

    fix layout

--- src/net/coagulate/SL/Pages/SetPassword.java
+++ src/net/coagulate/SL/Pages/SetPassword.java
@@ -13,8 +13,8 @@ public class SetPassword extends Page {
         pageHeader("Set Password");
         
         centralisePage();
-        para("Here you may set a password which you may use to log directly in to this application without having to log in to Second Life and message a bot.");
-        para("As a matter of general security you should use a unique password, and definately <b>NOT</b> the same password you use for Second Life");
+        para("Here you may set a password which you may use to log directly in to this application<br>without having to log in to Second Life and message a bot.");
+        para("As a matter of general security you should use a unique password,<br>and definately <b>NOT</b> the same password you use for Second Life");
         linebreak();
         startForm();
         label("New Password:").passwordInput("password");

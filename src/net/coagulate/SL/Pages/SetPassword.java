fde2f390a82492e9d53572070f1114312a5fe11c
Iain Price (LUNA) <git@predestined.net>
2018-09-20 00:36:26 +0100

    set password junction

--- src/net/coagulate/SL/Pages/SetPassword.java
+++ src/net/coagulate/SL/Pages/SetPassword.java
@@ -1,6 +1,7 @@
 package net.coagulate.SL.Pages;
 
 import net.coagulate.SL.HTTPPipelines.Page;
+import net.coagulate.SL.HTTPPipelines.State;
 
 /**
  *
@@ -13,6 +14,13 @@ public class SetPassword extends Page {
         pageHeader("Set Password");
         
         centralisePage();
+        State state=State.get();
+        if (state.get("Set Password").equals("Set Password"))
+        {
+            para("Setting your password to "+state.get("password"));
+            para("Have a nice day :P");
+            return;
+        }
         para("Here you may set a password which you may use to log directly in to this application<br>without having to log in to Second Life and message a bot.");
         para("As a matter of general security you should use a unique password,<br>and definately <b>NOT</b> the same password you use for Second Life");
         linebreak();

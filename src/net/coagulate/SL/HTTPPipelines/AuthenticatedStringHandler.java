9091998c6dcf27670076a6261a89c3a4783dbe64
Iain Price (LUNA) <git@predestined.net>
2018-09-20 01:08:05 +0100

    needs to be in a form...

--- src/net/coagulate/SL/HTTPPipelines/AuthenticatedStringHandler.java
+++ src/net/coagulate/SL/HTTPPipelines/AuthenticatedStringHandler.java
@@ -15,8 +15,8 @@ public abstract class AuthenticatedStringHandler extends StringHandler {
         State state=State.get();
         if (state.user!=null) { return handleAuthenticated(); }
         // not (yet?) logged in
-        String username=state.get("username");
-        String password=state.get("password");
+        String username=state.get("login_username");
+        String password=state.get("login_password");
         if (state.get("Login").equals("Login") && !username.isEmpty() && !password.isEmpty()) {
             User u=User.get(username, false);
             if (u==null) {
@@ -33,9 +33,9 @@ public abstract class AuthenticatedStringHandler extends StringHandler {
         return loginpage1+loginpage2;
     }
     
-    private static final String loginpage1="<p align=center><table><tr><td colspan=2>&nbsp;</td></tr><tr><td></td><td colspan=2 align=center><font size=5><u>Login</u></font></td></tr>";
-    private static final String loginpage2="<tr><th>Username:</th><td><input type=text size=20 name=username></td></tr>"
-            + "<tr><th>Password:</th><td><input type=password size=20 name=password></td></tr>"
+    private static final String loginpage1="<form method=post><p align=center><table><tr><td colspan=2>&nbsp;</td></tr><tr><td></td><td colspan=2 align=center><font size=5><u>Login</u></font></td></tr>";
+    private static final String loginpage2="<tr><th>Username:</th><td><input type=text size=20 name=login_username></td></tr>"
+            + "<tr><th>Password:</th><td><input type=password size=20 name=login_password></td></tr>"
             + "<tr><th></th><td><i><b>NOT</b> your SL password</i></td></tr>"
             + "<tr><td colspan=2>&nbsp;</td></tr>"
             + "<tr><td></td><td><button type=submit name=Login value=Login style='width:100%;'>Login</button></td></tr>"
@@ -47,7 +47,7 @@ public abstract class AuthenticatedStringHandler extends StringHandler {
             + "===> Click <a href=\"secondlife:///app/agent/"+Launch.bot.getUUID().toUUIDString()+"/im\">to instant message the bot "+Launch.bot.getUsername()+"</a><br>"
             + "Send the message 'login', and the bot will reply with a URL that will log you in.</p>"
             + "<p>If you wish to avoid the Second Life step in future, and use a password, follow the above to get logged in, and then click 'Set Password' on the top right of the web pages</p></td></tr>"
-            + "</table></p>";
+            + "</table></p></form>";
             
     
     public abstract String handleAuthenticated();

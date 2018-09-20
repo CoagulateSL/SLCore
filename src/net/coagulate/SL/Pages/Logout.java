be843f388c8753a0f0eb7088ff8405e9090a6121
Iain Price (LUNA) <git@predestined.net>
2018-09-20 01:38:33 +0100

    logout functionality

---
+++ src/net/coagulate/SL/Pages/Logout.java
@@ -0,0 +1,18 @@
+package net.coagulate.SL.Pages;
+
+import net.coagulate.SL.HTTPPipelines.State;
+import net.coagulate.SL.HTTPPipelines.StringHandler;
+
+/**
+ *
+ * @author Iain Price
+ */
+public class Logout extends StringHandler {
+
+    @Override
+    protected String handleString() {
+        State.get().logout();
+        return "<br><br><h3 align=center>Your session has been ended</h3><br><br><br><p align=center><a href=\"/\">Click here to return to the login page</a></p>";
+    }
+    
+}

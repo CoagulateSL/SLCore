65908a60f1c6ac0366d9763ecc3d9b9c15e64be6
Iain Price (LUNA) <git@predestined.net>
2018-09-22 14:39:14 +0100

    test index page

--- src/net/coagulate/SL/Pages/Index.java
+++ src/net/coagulate/SL/Pages/Index.java
@@ -16,9 +16,19 @@ public class Index extends Page {
         para("Welcome to Coagulate Second Life services");
         raw("<table style=\"max-width: 700px;\" align=center>");
         raw("<tr width=100%>");
-        raw("<td width=33%><h3>Tab1</h3></td>");
-        raw("<td width=33%><h3>Tab2</h3></td>");
-        raw("<td width=33%><h3>Tab3</h3></td>");
+        raw("<td style=\"border-width: 1px; border-style: solid;\" width=33%>");
+        raw("<h3>Tab1</h3>");
+        p("Region Monitoring monitors the status of specified regions, polling every minute and logging the data.");
+        p("This data can be reviewed on the website, or linked to in-world objects to provide region status updates.");
+        raw("</td>");
+        raw("<td style=\"border-width: 1px; border-style: solid;\" width=33%>");
+        raw("<h3>Tab2</h3>");
+        p("Region Statistics monitors various aspects of region performance such as FPS, script info, etc");
+        p("This information is presented through the website and is graphable");
+        raw("</td>");
+        raw("<td style=\"border-width: 1px; border-style: solid;\" width=33%>");
+        raw("<h3>Tab3</h3>");
+        raw("</td>");
         raw("</tr>");
         raw("</table>");
     }

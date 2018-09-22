40cf020e2fdec7c73d14080744a7b6da7c2c2ee8
Iain Price (LUNA) <git@predestined.net>
2018-09-22 16:02:46 +0100

    test index page

--- src/net/coagulate/SL/Pages/Index.java
+++ src/net/coagulate/SL/Pages/Index.java
@@ -14,15 +14,15 @@ public class Index extends Page {
         para("Welcome to Coagulate Second Life services, select a service for more information.");
         raw("<table style=\"max-width: 900px;\" align=center>");
         raw("<tr width=100%><td width=100%><ul style=\"white-space: nowrap;\">");
-        openServiceCell("Region Monitoring");
+        openServiceCell("Region Monitoring","/RegionMonitor");
         p("Region Monitoring monitors the status of specified regions, polling every minute and logging the data.");
         p("This data can be reviewed on the website, or linked to in-world objects to provide region status updates.");
         closeServiceCell();
-        openServiceCell("Region Performance");
+        openServiceCell("Region Performance","/RegionPerformance");
         p("Region Statistics monitors various aspects of region performance such as FPS, script info, etc");
         p("This information is presented through the website and is graphable");
         closeServiceCell();
-        openServiceCell("Bot Agents");
+        openServiceCell("Bot Agents","/Bot");
         p("This allows you to operate an automated agent in Second Life (a 'bot').");
         p("This can be used to automate group invites, group ejects, and other features");
         closeServiceCell();
@@ -30,9 +30,10 @@ public class Index extends Page {
         raw("</table>");
     }
 
-    private void openServiceCell(String title) {
-        raw("<li style=\"vertical-align: top; white-space: normal; color:black; border-style: solid; border-width: 5px; width: 200px; text-align: center; margin: 0px; list-style-type: none; display: inline-block;\">");
+    private void openServiceCell(String title,String target) {
+        raw("<a href=\""+target+"\">");
+        raw("<li style=\"vertical-align: top; white-space: normal; color:black; border-style: solid; border-width: 5px; height: 250px; width: 200px; text-align: center; margin: 0px; list-style-type: none; display: inline-block;\">");
         raw("<h3 align=center>"+title+"</h3>");        
     }
-    private void closeServiceCell() { raw("</li>"); }
+    private void closeServiceCell() { raw("</li></a>"); }
 }

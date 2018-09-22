dda11767b87c0e5440c6ab87d2c92782f56c6e94
Iain Price (LUNA) <git@predestined.net>
2018-09-22 14:33:32 +0100

    index page

--- src/net/coagulate/SL/Pages/Index.java
+++ src/net/coagulate/SL/Pages/Index.java
@@ -14,6 +14,13 @@ public class Index extends Page {
         centralisePage();
         pageHeader("Welcome");
         para("Welcome to Coagulate Second Life services");
+        raw("<table style=\"max-width: 700px;\" align=center>");
+        raw("<tr width=100%>");
+        raw("<td width=33%><h3>Tab1</h3></td>");
+        raw("<td width=33%><h3>Tab2</h3></td>");
+        raw("<td width=33%><h3>Tab3</h3></td>");
+        raw("</tr>");
+        raw("</table>");
     }
     
 }

f4101dbddea001773996d5cc61e71cac6f4d9c24
Iain Price (LUNA) <git@predestined.net>
2018-09-22 14:46:06 +0100

    test index page

--- src/net/coagulate/SL/Pages/Index.java
+++ src/net/coagulate/SL/Pages/Index.java
@@ -16,21 +16,25 @@ public class Index extends Page {
         para("Welcome to Coagulate Second Life services");
         raw("<table style=\"max-width: 700px;\" align=center>");
         raw("<tr width=100%>");
-        raw("<td style=\"vertical-align: top; border-width: 1px; border-style: solid;\" width=33%>");
-        raw("<h3 align=center>Tab1</h3>");
+        openServiceCell("Region Monitoring");
         p("Region Monitoring monitors the status of specified regions, polling every minute and logging the data.");
         p("This data can be reviewed on the website, or linked to in-world objects to provide region status updates.");
-        raw("</td>");
-        raw("<td style=\"vertical-align: top; border-width: 1px; border-style: solid;\" width=33%>");
-        raw("<h3 align=center>Tab2</h3>");
+        closeServiceCell();
+        openServiceCell("Region Performance");
         p("Region Statistics monitors various aspects of region performance such as FPS, script info, etc");
         p("This information is presented through the website and is graphable");
-        raw("</td>");
-        raw("<td style=\"vertical-align: top; border-width: 1px; border-style: solid;\" width=33%>");
-        raw("<h3 align=center>Tab3</h3>");
-        raw("</td>");
+        closeServiceCell();
+        openServiceCell("Bot Agents");
+        p("This allows you to operate an automated agent in Second Life (a 'bot').");
+        p("This can be used to automate group invites, group ejects, and other features");
+        closeServiceCell();
         raw("</tr>");
         raw("</table>");
     }
-    
+
+    private void openServiceCell(String title) {
+        raw("<td style=\"padding: 10px; vertical-align: top; border-width: 1px; border-style: solid;\" width=33%>");
+        raw("<h3 align=center>"+title+"</h3>");        
+    }
+    private void closeServiceCell() { raw("</td>"); }
 }

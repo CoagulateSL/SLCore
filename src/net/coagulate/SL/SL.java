42ce2f9fbadd62794ac99e818c844000b78285f6
Iain Price (LUNA) <git@predestined.net>
2018-10-11 00:51:54 +0100

    Move to DB framework

--- src/net/coagulate/SL/SL.java
+++ src/net/coagulate/SL/SL.java
@@ -3,10 +3,12 @@ package net.coagulate.SL;
 import static java.util.logging.Level.SEVERE;
 import java.util.logging.Logger;
 import net.coagulate.Core.ClassTools;
+import net.coagulate.Core.Database.DB;
+import net.coagulate.Core.Database.DBConnection;
+import net.coagulate.Core.Database.MariaDBConnection;
 import net.coagulate.Core.LogHandler;
 import net.coagulate.JSLBot.JSLBot;
 import net.coagulate.JSLBot.LLCATruster;
-import net.coagulate.SL.Database.Database;
 
 /** Bootstrap class.
  *
@@ -20,6 +22,7 @@ public class SL extends Thread {
     public static JSLBot bot;
     private static boolean shutdown=false;
     private static boolean errored=false;
+    private static DBConnection db;
     
     public static void shutdown() { shutdown=true; }
     
@@ -38,7 +41,7 @@ public class SL extends Thread {
         Pricing.initialise();
         LLCATruster.doNotUse();
         CATruster.initialise();
-        Database.initialise();
+        db=new MariaDBConnection("SL",Config.getJdbc());
         IPC.test();
         startBot(); 
         HTTPSListener.initialise();
@@ -48,7 +51,7 @@ public class SL extends Thread {
         log.config("SL Services shutting down");
         HTTPSListener.blockingShutdown();
         if (bot!=null) { bot.shutdown("SL System is shutting down"); }
-        Database.shutdown();
+        DB.shutdown();
         log.config("SL Services shutdown is complete, exiting.");
         if (errored) { System.exit(1); }
         System.exit(0);
@@ -66,7 +69,7 @@ public class SL extends Thread {
     public static void watchdog() {
         try { Thread.sleep(1000); } catch (InterruptedException e) {}
         if (shutdown) return;
-        if (!Database.test()) {
+        if (!DB.test()) {
             log.log(SEVERE,"Database failed connectivity test, shutting down."); shutdown=true; errored=true;
         }
         if (shutdown) return;
@@ -80,6 +83,8 @@ public class SL extends Thread {
         LogHandler.initialise();
         log=Logger.getLogger("net.coagulate.SL");        
     }
+
+    public static DBConnection getDB() { return db; }
 
     private SL() {}
     @Override

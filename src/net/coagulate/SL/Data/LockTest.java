e7534827885ef9501741d3a017aef61dc3e855eb
Iain Price (LUNA) <git@predestined.net>
2018-10-15 03:47:47 +0100

    call gphud maintenance, use cluster lock

--- src/net/coagulate/SL/Data/LockTest.java
+++ src/net/coagulate/SL/Data/LockTest.java
@@ -1,12 +1,18 @@
 package net.coagulate.SL.Data;
 
+import net.coagulate.Core.Tools.SystemException;
+
 /**
  *
  * @author Iain Price
  */
 public class LockTest extends LockableTable {
 
-    public LockTest(int id) { super(id); }
+    public LockTest(int id) {
+        super(id);
+        Integer count=dqi(true,"select count(*) from locktest where id=?",id);
+        if (count==0) { throw new SystemException("Lock test row "+id+" does not exist"); }
+    }
 
     @Override
     public String getTableName() { return "locktest"; }

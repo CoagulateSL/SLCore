862b3a45b3941c5e9ff9cc96a06c33a8b07746c0
Iain Price (LUNA) <git@predestined.net>
2018-10-17 20:57:05 +0100

    404

---
+++ src/net/coagulate/SL/Pages/FourZeroFour.java
@@ -0,0 +1,16 @@
+package net.coagulate.SL.Pages;
+
+import net.coagulate.SL.HTTPPipelines.StringHandler;
+
+/**
+ *
+ * @author Iain Price
+ */
+public class FourZeroFour extends StringHandler {
+
+    @Override
+    protected String handleString() {
+        return "<h1 align=center>Four Hundred and Four</h1><br><br><p align=center>As in, 404, Page Not Found</p><br><br><br><br><p align=center>The requested URI was not mapped to a page handler.</p>";
+    }
+    
+}

ab1352306a38b60767d19a8bc8215a0b47e698fe
Iain Price (LUNA) <git@predestined.net>
2018-10-17 04:34:27 +0100

    basic URL handling evolving

---
+++ src/net/coagulate/SL/HTTPPipelines/PageMapper.java
@@ -0,0 +1,43 @@
+package net.coagulate.SL.HTTPPipelines;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+import org.apache.http.HttpRequest;
+import org.apache.http.protocol.HttpRequestHandler;
+import org.apache.http.protocol.HttpRequestHandlerMapper;
+
+/**
+ *
+ * @author Iain Price
+ */
+public class PageMapper implements HttpRequestHandlerMapper {
+
+    public Logger logger;
+    
+    Map<String,HttpRequestHandler> prefixes=new HashMap<>();
+    Map<String,HttpRequestHandler> exact=new HashMap<>();
+            
+    public void exact(String url,HttpRequestHandler handler) { exact.put(url.toLowerCase(),handler); }
+    public void prefix(String url,HttpRequestHandler handler) { prefixes.put(url.toLowerCase(),handler); }
+            
+    public PageMapper() {
+        logger=Logger.getLogger(PageMapper.class.getCanonicalName());
+
+        // GPHUD mappings
+        net.coagulate.GPHUD.Interface.base="GPHUD";
+        exact("/GPHUD/system",new net.coagulate.GPHUD.Interfaces.System.Interface());
+        prefix("/GPHUD/",new net.coagulate.GPHUD.Interfaces.User.Interface());
+        prefix("/GPHUD/hud/",new net.coagulate.GPHUD.Interfaces.HUD.Interface());
+        
+        
+    }
+    
+    
+    @Override
+    public HttpRequestHandler lookup(HttpRequest req) {
+        System.out.println("REQUEST:"+req.getRequestLine());
+        return null;
+    }
+    
+}

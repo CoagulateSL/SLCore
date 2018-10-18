9bc4770339be2ef98127e6e2bca6a600c81d36a3
Iain Price (LUNA) <git@predestined.net>
2018-10-18 18:24:05 +0100

    log status changes

---
+++ src/net/coagulate/SL/SecondLifeAPI/RegisterURL.java
@@ -0,0 +1,19 @@
+package net.coagulate.SL.SecondLifeAPI;
+
+import net.coagulate.SL.HTTPPipelines.PageMapper.Url;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Iain Price
+ */
+public class RegisterURL extends SLAPI {
+
+    @Url("/SecondLifeAPI/RegisterURL")
+    public RegisterURL(){super();}
+    @Override
+    protected JSONObject handleJSON(JSONObject object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

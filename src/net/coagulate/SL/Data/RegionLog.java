7f6edd4c8ffd1d014bef87332a5b3ebef26d5c12
Iain Price (LUNA) <git@predestined.net>
2018-11-30 18:36:12 +0000

    param mismatch

--- src/net/coagulate/SL/Data/RegionLog.java
+++ src/net/coagulate/SL/Data/RegionLog.java
@@ -13,7 +13,7 @@ public class RegionLog extends Table {
     public String getTableName() { return "regionlog"; }
     
     public static void log(int region,String changetype,String oldvalue,String newvalue,int tds) {
-        SL.getDB().d("insert into regionlog(regionid,changetype,oldvalue,newvalue,tds) values(?,?,?,?)",region,changetype,oldvalue,newvalue,tds);
+        SL.getDB().d("insert into regionlog(regionid,changetype,oldvalue,newvalue,tds) values(?,?,?,?,?)",region,changetype,oldvalue,newvalue,tds);
     }
     public static void log(Regions region,String changetype,String oldvalue,String newvalue,int tds) {
         log(region.getId(),changetype,oldvalue,newvalue,tds);

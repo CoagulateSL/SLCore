49bc81bf05a4e8501a5921ee681a357b227084d4
Iain Price (LUNA) <git@predestined.net>
2018-11-30 18:32:58 +0000

    new status change table.  log changing version elements

--- src/net/coagulate/SL/Data/RegionLog.java
+++ src/net/coagulate/SL/Data/RegionLog.java
@@ -1,5 +1,6 @@
 package net.coagulate.SL.Data;
 
+import net.coagulate.Core.Tools.UnixTime;
 import net.coagulate.SL.SL;
 
 /**
@@ -11,8 +12,18 @@ public class RegionLog extends Table {
     @Override
     public String getTableName() { return "regionlog"; }
     
-    public static void log(int region,String changetype,String status,int from,int until) {
-        SL.getDB().d("insert into regionlog(regionid,changetype,status,begin,end) values(?,?,?,?)",region,changetype,status,from,until);
+    public static void log(int region,String changetype,String oldvalue,String newvalue,int tds) {
+        SL.getDB().d("insert into regionlog(regionid,changetype,oldvalue,newvalue,tds) values(?,?,?,?)",region,changetype,oldvalue,newvalue,tds);
+    }
+    public static void log(Regions region,String changetype,String oldvalue,String newvalue,int tds) {
+        log(region.getId(),changetype,oldvalue,newvalue,tds);
+    }
+
+    public static void log(int region,String changetype,String oldvalue,String newvalue) {
+        log(region,changetype,oldvalue,newvalue,UnixTime.getUnixTime());
+    }
+    public static void log(Regions region,String changetype,String oldvalue,String newvalue) {
+        log(region.getId(),changetype,oldvalue,newvalue,UnixTime.getUnixTime());
     }
     
 }

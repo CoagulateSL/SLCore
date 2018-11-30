55a5d3593506d63c1e900f779259695679562b6d
Iain Price (LUNA) <git@predestined.net>
2018-11-30 19:07:27 +0000

    log region stats

---
+++ src/net/coagulate/SL/Data/RegionStats.java
@@ -0,0 +1,21 @@
+package net.coagulate.SL.Data;
+
+import net.coagulate.SL.SL;
+
+/**
+ *
+ * @author Iain Price
+ */
+public class RegionStats extends Table {
+
+    @Override
+    public String getTableName() { return "regionstats"; }
+    
+    public static void log(int region,int timestamp,String statstype,float min,float max,float avg,float sd) {
+        SL.getDB().d("insert into regionstats(regionid,timestamp,statstype,statmin,statmax,statsavg,statsd) values(?,?,?,?,?,?)",region,timestamp,statstype,min,max,avg,sd);
+    }
+    public static void log(Regions region,int timestamp,String statstype,float min,float max,float avg,float sd) {
+        log(region.getId(),timestamp,statstype,min,max,avg,sd);
+    }
+    
+}

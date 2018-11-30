837a0b0b437eb12dec63d4816ba1861ca1f1f742
Iain Price (LUNA) <git@predestined.net>
2018-11-30 15:19:51 +0000

    test path for mailer

---
+++ src/net/coagulate/SL/Pages/ControlPanel.java
@@ -0,0 +1,46 @@
+package net.coagulate.SL.Pages;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.MessagingException;
+import net.coagulate.Core.Tools.ExceptionTools;
+import net.coagulate.Core.Tools.MailTools;
+import net.coagulate.Core.Tools.SystemException;
+import net.coagulate.SL.Config;
+import net.coagulate.SL.HTTPPipelines.Page;
+import net.coagulate.SL.HTTPPipelines.PageMapper.Url;
+import net.coagulate.SL.HTTPPipelines.State;
+
+/**
+ *
+ * @author Iain Price
+ */
+public class ControlPanel extends Page {
+
+    @Url("/ControlPanel")
+    public ControlPanel(){super();}
+    @Override
+    public void content() {
+        if (!State.get().user().superuser()) {
+            throw new SystemException("Unauthorised access to Control Panel from "+State.get().user());
+        }
+        pageHeader("Control Panel");
+        centralisePage();
+        State state=State.get();
+        if (state.get("Test Mail").equals("Test Mail"))
+        {
+            para("Sending mail");
+            try { MailTools.mail("CL Cluster "+Config.getHostName(),"sl-cluster-alerts@predestined.net","SL Mail Tester", "sl-cluster-alerts@predestined.net", "SL Cluster mail test", "Test OK"); }
+            catch (MessagingException ex) {
+                Logger.getLogger(ControlPanel.class.getName()).log(Level.SEVERE, null, ex);
+                raw(ExceptionTools.toHTML(ex));
+            }
+            para("Sent mail");
+        }
+        startForm();
+        submit("Test Mail");
+        endForm();
+    }
+
+    
+}

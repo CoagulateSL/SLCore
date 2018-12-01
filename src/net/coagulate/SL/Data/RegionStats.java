b91158b3d58924ac20f101d4671c0debf3ce74ac
Iain Price (LUNA) <git@predestined.net>
2018-12-01 23:37:33 +0000

    commit values as single explicitly (for the aggregation component)

--- src/net/coagulate/SL/Data/RegionStats.java
+++ src/net/coagulate/SL/Data/RegionStats.java
@@ -13,7 +13,7 @@ public class RegionStats extends Table {
     
     public static void log(int region,int timestamp,String statstype,float min,float max,float avg,float sd) {
         //SL.getLogger("RegionStas").fine("Region "+region+" has timestamp "+timestamp+" with stats "+statstype+"/"+min+"/"+max+"/"+avg+"/"+sd);
-        SL.getDB().d("insert into regionstats(regionid,timestamp,stattype,statmin,statmax,statavg,statsd) values(?,?,?,?,?,?,?)",region,timestamp,statstype,min,max,avg,sd);
+        SL.getDB().d("insert into regionstats(regionid,timestamp,stattype,statmin,statmax,statavg,statsd,samplesize) values(?,?,?,?,?,?,?,?)",region,timestamp,statstype,min,max,avg,sd,"SINGLE");
     }
     public static void log(Regions region,int timestamp,String statstype,float min,float max,float avg,float sd) {
         log(region.getId(),timestamp,statstype,min,max,avg,sd);

984fe0d18a6140248c1e6de8059ae83b5488c0e7
Iain Price (LUNA) <git@predestined.net>
2018-12-01 02:19:18 +0000

    silence debugging

--- src/net/coagulate/SL/Data/RegionStats.java
+++ src/net/coagulate/SL/Data/RegionStats.java
@@ -12,7 +12,7 @@ public class RegionStats extends Table {
     public String getTableName() { return "regionstats"; }
     
     public static void log(int region,int timestamp,String statstype,float min,float max,float avg,float sd) {
-        SL.getLogger("RegionStas").fine("Region "+region+" has timestamp "+timestamp+" with stats "+statstype+"/"+min+"/"+max+"/"+avg+"/"+sd);
+        //SL.getLogger("RegionStas").fine("Region "+region+" has timestamp "+timestamp+" with stats "+statstype+"/"+min+"/"+max+"/"+avg+"/"+sd);
         SL.getDB().d("insert into regionstats(regionid,timestamp,stattype,statmin,statmax,statavg,statsd) values(?,?,?,?,?,?,?)",region,timestamp,statstype,min,max,avg,sd);
     }
     public static void log(Regions region,int timestamp,String statstype,float min,float max,float avg,float sd) {

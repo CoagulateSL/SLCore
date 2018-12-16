047c1c5c0922a66681eb279163e7bb70190269ec
Iain Price (LUNA) <git@predestined.net>
2018-12-16 20:17:51 +0000

    basic bot table interface

---
+++ src/net/coagulate/SL/Data/Bots.java
@@ -0,0 +1,23 @@
+package net.coagulate.SL.Data;
+
+/**
+ *
+ * @author Iain Price
+ */
+public class Bots extends LockableTable {
+
+    public Bots(int id) {
+        super(id);
+    }
+
+    @Override
+    public String getTableName() {
+        return "bots";
+    }
+    
+    public String getFirstName() { return getString("firstname"); }
+    public String getLastName() { return getString("lastname"); }
+    public String getPassword() { return getString("password"); }
+    public User getOwner() { return User.get(getInt("ownerid")); }
+    
+}

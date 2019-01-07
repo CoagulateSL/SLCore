72e225ff45c8a953f7249d9c7e93cc478e278fcb
Iain Price (LUNA) <git@predestined.net>
2019-01-07 02:56:28 +0000

    invert comparator

--- src/net/coagulate/SL/Pages/HTML/TimeStampOrDuration.java
+++ src/net/coagulate/SL/Pages/HTML/TimeStampOrDuration.java
@@ -12,7 +12,7 @@ public class TimeStampOrDuration extends TimeStamp {
         int value=Integer.parseInt(strvalue);
         int now=UnixTime.getUnixTime();
         int diff=now-value;
-        if (diff<(-4*UnixTime.WEEK) || diff>(4*UnixTime.WEEK)) {
+        if (diff>(-4*UnixTime.WEEK) && diff<(4*UnixTime.WEEK)) {
             return UnixTime.durationRelativeToNow(value);
         }
         return super.render(st,strvalue);

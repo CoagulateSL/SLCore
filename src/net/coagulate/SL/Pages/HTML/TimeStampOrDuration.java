7efd3942287d2c4de32702bcc550d120a3de847a
Iain Price (LUNA) <git@predestined.net>
2019-01-07 03:00:43 +0000

    think more :/

--- src/net/coagulate/SL/Pages/HTML/TimeStampOrDuration.java
+++ src/net/coagulate/SL/Pages/HTML/TimeStampOrDuration.java
@@ -12,7 +12,7 @@ public class TimeStampOrDuration extends TimeStamp {
         int value=Integer.parseInt(strvalue);
         int now=UnixTime.getUnixTime();
         int diff=now-value;
-        boolean historic=false; if (now<value) { historic=true; }
+        boolean historic=false; if (now>value) { historic=true; }
         if (diff>(-4*UnixTime.WEEK) && diff<(4*UnixTime.WEEK)) {
             return (historic?"":"in ")+UnixTime.durationRelativeToNow(value,false)+(historic?" ago":"");
         }

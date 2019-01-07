089040bdb7876048a28e6db7a6c8ecd81bfcd29d
Iain Price (LUNA) <git@predestined.net>
2019-01-07 02:53:56 +0000

    TimeStamp or duration (if recent!)

---
+++ src/net/coagulate/SL/Pages/HTML/TimeStampOrDuration.java
@@ -0,0 +1,20 @@
+package net.coagulate.SL.Pages.HTML;
+
+import net.coagulate.Core.Tools.UnixTime;
+
+/**
+ *
+ * @author Iain Price
+ */
+public class TimeStampOrDuration extends TimeStamp {
+    public String render(State st,String strvalue) {
+        if (value==null || value.isEmpty() || Integer.parseInt(value)==0) { return "-"; }
+        int value=Integer.parseInt(strvalue);
+        int now=UnixTime.getUnixTime();
+        int diff=now-value;
+        if (diff<(-4*UnixTime.WEEK) || diff>(4*UnixTime.WEEK)) {
+            return UnixTime.durationRelativeToNow(value);
+        }
+        return super.render(st,strvalue);
+    }
+}

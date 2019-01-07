35d18207237022f54092d7275aef14dbe58f7835
Iain Price (LUNA) <git@predestined.net>
2019-01-07 02:59:53 +0000

    imprecise with in/ago

--- src/net/coagulate/SL/Pages/HTML/TimeStampOrDuration.java
+++ src/net/coagulate/SL/Pages/HTML/TimeStampOrDuration.java
@@ -12,8 +12,9 @@ public class TimeStampOrDuration extends TimeStamp {
         int value=Integer.parseInt(strvalue);
         int now=UnixTime.getUnixTime();
         int diff=now-value;
+        boolean historic=false; if (now<value) { historic=true; }
         if (diff>(-4*UnixTime.WEEK) && diff<(4*UnixTime.WEEK)) {
-            return UnixTime.durationRelativeToNow(value,false);
+            return (historic?"":"in ")+UnixTime.durationRelativeToNow(value,false)+(historic?" ago":"");
         }
         return super.render(st,strvalue);
     }

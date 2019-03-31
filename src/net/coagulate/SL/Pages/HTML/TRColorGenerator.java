092259379515daa19b2ad2812a5eec1da6add2d5
Iain Price (LUNA) <git@predestined.net>
2019-03-31 02:23:20 +0100

    uniform spelling of colour as color

---
+++ src/net/coagulate/SL/Pages/HTML/TRColorGenerator.java
@@ -0,0 +1,28 @@
+package net.coagulate.SL.Pages.HTML;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Iain Price
+ */
+public class TRColorGenerator extends TRGenerator {
+
+    private final String columnname;
+    private final String defaultcolor;
+    public TRColorGenerator(String columnname,String defaultcolor) { this.columnname=columnname; this.defaultcolor=defaultcolor; }
+
+    Map<String,String> valuemap=new HashMap<>();
+    public TRColorGenerator map(String value,String color) { valuemap.put(value,color); return this; }
+    @Override
+    public String render(State st, Map<String, String> row) {
+        return "<tr bgcolor=\"#"+getColor(st,row)+"\">";
+    }
+    private String getColor(State st,Map<String,String> row) {
+        if (!row.containsKey(columnname)) { return defaultcolor; }
+        String value=row.get(columnname);
+        if (!valuemap.containsKey(value)) { return defaultcolor; }
+        return valuemap.get(value);
+    }
+}

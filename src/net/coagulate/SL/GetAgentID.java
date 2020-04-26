f4fbc99185954ad1a1429688067eb85f182a8aba
Iain Price (LUNA) <git@predestined.net>
2020-04-26 15:56:27 +0100

    LL Name lookup service driver

--- src/net/coagulate/SL/GetAgentID.java
+++ src/net/coagulate/SL/GetAgentID.java
@@ -1,10 +1,17 @@
 package net.coagulate.SL;
 
+import net.coagulate.Core.Exceptions.System.SystemRemoteFailureException;
 import net.coagulate.Core.Exceptions.User.UserInputValidationFilterException;
+import net.coagulate.Core.Exceptions.User.UserRemoteFailureException;
+import org.json.JSONObject;
 
 import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.regex.Pattern;
 
 public class GetAgentID {
@@ -12,7 +19,8 @@ public class GetAgentID {
 	private static final String SERVICE_URL="https://api.secondlife.com/get_agent_id";
 	private static final String API_KEY="***REMOVED***";
 
-// ---------- STATICS ----------
+	// ---------- STATICS ----------
+
 	/**
 	 * Resolve a name to UUID.
 	 * Exceptions.  A LOT.  Exceptions should be taken as advisory not fatal.
@@ -24,83 +32,84 @@ public class GetAgentID {
 	@Nonnull
 	public static final String getAgentID(
 			@Nonnull
-			final String name) throws Exception {
+			final String name) {
 
 		// validate the name a bit and we need to break it down into a firstname (called username in modern LL nomenclature, apparently)
 		// and a last name
 		// "Usernames can contain only letters and numbers" (and when concatenated a single space or dot separator)
-		if (Pattern.compile("[^A-Za-z0-9\\. ]?").matcher(name).matches()) {
+		if (Pattern.compile(".*[^A-Za-z0-9\\. ].*").matcher(name).matches()) {
 			throw new UserInputValidationFilterException("Name '"+name+"' contains invalid characters");
 		}
-		if (Pattern.compile("([ \\.])").matcher(name).groupCount()>1) {
+		if (Pattern.compile(".*[ \\.].*[ \\.].*").matcher(name).matches()) {
 			throw new UserInputValidationFilterException("Name '"+name+"' contains too many separators");
 		}
 
+		String firstname=name.replaceAll("\\."," ");
+		String lastname=null;
+		if (firstname.contains(" ")) {
+			String[] parts=firstname.split(" ");
+			firstname=parts[0];
+			lastname=parts[1];
+		}
 
-		final URLConnection transmission=new URL(SERVICE_URL).openConnection();
-		transmission.setDoOutput(true);
-		transmission.setAllowUserInteraction(false);
-		transmission.setDoInput(true);
-		transmission.setConnectTimeout(5000);
-		transmission.setReadTimeout(35000);
-		transmission.setRequestProperty("api-key", API_KEY);
-		transmission.connect();
-/*
-		JSONObject request=new JSONObject();
-		request.put("username",firstname);
-		request.put("lastname",lastname);
+		try {
+			final HttpURLConnection transmission=((HttpURLConnection)new URL(SERVICE_URL).openConnection());
+			transmission.setDoOutput(true);
+			transmission.setAllowUserInteraction(false);
+			transmission.setDoInput(true);
+			transmission.setConnectTimeout(2000);
+			transmission.setReadTimeout(2000);
+			transmission.setRequestProperty("api-key",API_KEY);
+			transmission.connect();
 
-		final OutputStreamWriter out=new OutputStreamWriter(transmission.getOutputStream());
-		out.write(request+"\n");
-		out.flush();
-		out.close();
-		final BufferedReader rd=new BufferedReader(new InputStreamReader(transmission.getInputStream()));
-		String line;
-		final StringBuilder response=new StringBuilder();
-		//noinspection NestedAssignment
-		while ((line=rd.readLine())!=null) {
-			response.append(line).append("\n");
-		}
-		if (response==null) {
-			GPHUD.getLogger().log(WARNING,"Failed all retransmission attempts for "+json);
-			return;
-		}
-		if (!response.isEmpty()) {
-			try {
-				final JSONObject j=new JSONObject(response);
-				jsonresponse=j;
-				final String incommand=j.optString("incommand","");
-				if ("pong".equals(incommand)) {
-					if (j.has("callback")) { Char.refreshURL(j.getString("callback")); }
-					if (j.has("callback")) { Region.refreshURL(j.getString("callback")); }
-					if (j.has("cookie")) { Cookie.refreshCookie(j.getString("cookie")); }
-				}
+			JSONObject request=new JSONObject();
+			request.put("username",firstname);
+			if (lastname!=null) { request.put("lastname",lastname); }
+
+			final OutputStreamWriter out=new OutputStreamWriter(transmission.getOutputStream());
+			out.write(request+"\n");
+			out.flush();
+			out.close();
+
+			int responsecode=900;
+			responsecode=transmission.getResponseCode();
+
+			switch (responsecode) {
+				case 403: throw new SystemRemoteFailureException("SL Name API Rate Limited");
+				case 405: throw new SystemRemoteFailureException("SL Name API said Malformed Request");
+				case 500: throw new UserRemoteFailureException("SL Name API service errored");
+				case 900: throw new SystemRemoteFailureException("SL Name API utility didn't get a response code");
+				case 901: throw new SystemRemoteFailureException("SL Name API utility gave a non numeric response code");
 			}
-			catch (
-					@Nonnull
-					final Exception e) {
-				GPHUD.getLogger().log(WARNING,"Exception in response parser",e);
-				final StringBuilder body=new StringBuilder(url+"\n<br>\n");
-				body.append("Character:").append(character==null?"null":character.getNameSafe()).append("\n<br>\n");
-				if (caller!=null) {
-					for (final StackTraceElement ele: caller) {
-						body.append("Caller: ").append(ele.getClassName()).append("/").append(ele.getMethodName()).append(":").append(ele.getLineNumber()).append("\n<br>\n");
-					}
-				}
-				body.append(response);
-				try {
-					MailTools.mail("Failed response",body.toString());
-				}
-				catch (
-						@Nonnull
-						final MessagingException ee) {
-					GPHUD.getLogger().log(SEVERE,"Mail exception in response parser exception handler",ee);
-				}
+
+			final BufferedReader rd;
+			if (responsecode==200) { rd=new BufferedReader(new InputStreamReader(transmission.getInputStream())); }
+			else { rd=new BufferedReader(new InputStreamReader(transmission.getErrorStream())); }
+			String line;
+			final StringBuilder responsebuilder=new StringBuilder();
+			//noinspection NestedAssignment
+			while ((line=rd.readLine())!=null) {
+				responsebuilder.append(line).append("\n");
 			}
+			final String response=responsebuilder.toString();
+			if (response==null) {
+				throw new UserRemoteFailureException("SL Name API gave no response");
+			}
+			if (response.isEmpty()) {
+				throw new UserRemoteFailureException("SL Name API gave empty response");
+			}
+
+			JSONObject json=new JSONObject(response);
+			if (responsecode==404) {
+				throw new UserRemoteFailureException("SL Name API - "+json.optString("error","???")+" - "+json.optString("message","NoErrorMessage"));
+			}
+
+			return json.getString("agent_id");
 		}
-		succeeded=true;
-*/
-		return "";
+		catch (IOException ex) {
+			SL.report("Name API IO Error",ex,null);
+			throw new UserRemoteFailureException("SL Name API IO Error: "+ex.getLocalizedMessage(),ex);
+		}
 	}
 
 }

255bac8c0d9ac9a53de8bf4c845fb9bbe6ff3392
Iain Price (LUNA) <git@predestined.net>
2020-09-23 19:15:54 +0100

    Specify description text's height to cause alignment

--- src/net/coagulate/SL/HTML/ServiceTile.java
+++ src/net/coagulate/SL/HTML/ServiceTile.java
@@ -26,15 +26,16 @@ public class ServiceTile extends Container {
 
     @Override
     public String toString() {
+        final String fixedHeight="130";
         StringBuilder s=new StringBuilder();
         if (url!=null) { s.append("<a href=\"").append(url).append("\">"); }
         s.append("<li style=\"vertical-align: top; white-space: normal; color:black; border-style: solid; border-width: 5px; min-height: 250px; width: 200px; text-align: center; margin: 5px; padding: 5px; list-style-type: none; display: inline-block;\">\n");
         if (imageUrl==null) { s.append("<h3 align=\"center\">").append(moduleName); }
         else { s.append("<h3 align=\"center\"><img src=\"").append(imageUrl).append("\">"); }
         s.append("</h3>");
-        s.append("<p>");
-        s.append(description);
-        s.append("</p>");
+        s.append("<p><table height="+fixedHeight+"px valign=top align=center><tr height="+fixedHeight+"px valign=top align=center><td height="+fixedHeight+"px valign=top align=center>");
+        s.append("<b>").append(moduleName).append("</b><br>&nbsp;<br>").append(description);
+        s.append("</td></tr></table></p>");
         s.append("<p style=\"text-align: left; margin: 0px; font-size: 10px;\">").append(version)
                 .append(" ").append(commitId)
                 //.append("<span style=\"float:center;\">").append("hello").append("</span>")

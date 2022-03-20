9e9801066c1f0cc1573d70f97b07925528924e14
Iain Price (LUNA) <git@predestined.net>
2022-03-20 14:23:34 +0000

    Do not use dq() where there is no result set

--- src/net/coagulate/SL/Data/EventQueue.java
+++ src/net/coagulate/SL/Data/EventQueue.java
@@ -29,7 +29,7 @@ import java.util.List;
 public class EventQueue extends StandardSLTable{
 
     public static List<EventQueue> getOutstandingEvents() {
-        SL.getDB().dq("delete from eventqueue where expires<UNIX_TIMESTAMP() and status like 'OK'");
+        SL.getDB().d("delete from eventqueue where expires<UNIX_TIMESTAMP() and status like 'OK'");
         List<EventQueue> set=new ArrayList<>();
         for (ResultsRow row:SL.getDB().dq("select * from eventqueue where expires>UNIX_TIMESTAMP() and claimed is null order by id asc")) {
             set.add(new EventQueue(row));

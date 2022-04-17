b181caf21049f50d5d8ed21493fbc4504bf2bbcf
Iain Price (LUNA) <git@predestined.net>
2022-04-18 00:20:05 +0100

    Simplify out if statements

--- src/net/coagulate/SL/Pages/HTML/TimeStampOrDuration.java
+++ src/net/coagulate/SL/Pages/HTML/TimeStampOrDuration.java
@@ -13,15 +13,16 @@ public class TimeStampOrDuration extends TimeStamp {
 	// ---------- INSTANCE ----------
 	public String render(@Nonnull final State st,
 	                     @Nullable final String strvalue) {
-		if (strvalue==null || strvalue.isEmpty() || Integer.parseInt(strvalue)==0) { return "-"; }
-		final int value=Integer.parseInt(strvalue);
-		final int now=UnixTime.getUnixTime();
-		final int diff=now-value;
-		boolean historic=false;
-		if (now>value) { historic=true; }
-		if (diff>(-4*UnixTime.WEEK) && diff<(4*UnixTime.WEEK)) {
-			return (historic?"":"in ")+UnixTime.durationRelativeToNow(value,false)+(historic?" ago":"");
+		if (strvalue == null || strvalue.isEmpty() || Integer.parseInt(strvalue) == 0) {
+			return "-";
 		}
-		return super.render(st,strvalue);
+		final int value = Integer.parseInt(strvalue);
+		final int now = UnixTime.getUnixTime();
+		final int diff = now - value;
+		boolean historic = now > value;
+		if (diff > (-4 * UnixTime.WEEK) && diff < (4 * UnixTime.WEEK)) {
+			return (historic ? "" : "in ") + UnixTime.durationRelativeToNow(value, false) + (historic ? " ago" : "");
+		}
+		return super.render(st, strvalue);
 	}
 }

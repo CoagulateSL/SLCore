550f9c263ba5b73ea98d0e3c908d37110614c000
iain <git@predestined.net>
2025-03-23 05:08:08 +0000

    More ignored mime types

--- src/net/coagulate/SL/SLCore.java
+++ src/net/coagulate/SL/SLCore.java
@@ -279,8 +279,7 @@ public class SLCore extends SLModule {
 		results+=Config.getHostName().toLowerCase()+" mariadb.cluster.updatemax "+updateMax+"\n";
 		try {
 			@SuppressWarnings("CallToRuntimeExec") final Process zabbix=Runtime.getRuntime()
-			                                                                   .exec(new String[] {
-					                                                                   "/usr/bin/zabbix_sender","-z",
+			                                                                   .exec(new String[] {"zabbix_sender","-z",
 					                                                                   Config.getZabbixServer(),"-i-"});
 			zabbix.getOutputStream().write(results.getBytes(StandardCharsets.UTF_8));
 			zabbix.getOutputStream().close();
